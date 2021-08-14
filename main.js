a27a0ed9e57d2322910e773395951023243d30e5
Yunyun Liu <celeph4is@gmail.com>
2021-08-14 13:05:44 -0700

    remove dummy data

--- main.js
+++ main.js
@@ -2,11 +2,7 @@
 
 const gallery = document.querySelector('main');
 
-const imageUrls = [
-  'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
-  'https://etcanada.com/wp-content/uploads/2016/06/58c0500b2ac4c342c6fc4ab096ada150.jpg?quality=80&strip=all&crop=0px%2C21px%2C500px%2C333px&resize=720%2C480',
-  'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'
-];
+const imageUrls = [];
 
 const imageUrlInput = document.querySelector('input');
 const addImageButton = document.querySelector('button');
@@ -19,7 +15,7 @@ const handleClick = function() {
     imageUrls.push(inputValue);
     inputValue = '';
     updateGallery();
-    console.log(imageUrls);
+    // console.log(imageUrls);
   }
 }
 
