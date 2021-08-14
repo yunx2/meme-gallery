c381a55bba91768e42852b563c0ec4c7be218995
Yunyun Liu <celeph4is@gmail.com>
2021-08-14 13:03:50 -0700

    complete meme gallery project

--- main.js
+++ main.js
@@ -11,18 +11,22 @@ const imageUrls = [
 const imageUrlInput = document.querySelector('input');
 const addImageButton = document.querySelector('button');
 
-addImageButton.addEventListener('click', () => {
+const handleClick = function() {
   let inputValue = imageUrlInput.value;
   if (inputValue === '') {
     console.log('add an url')
   } else {
     imageUrls.push(inputValue);
     inputValue = '';
-    console.log('img urls array:', imageUrls);
+    updateGallery();
+    console.log(imageUrls);
   }
-});
+}
+
+addImageButton.addEventListener('click', handleClick);
 
 function updateGallery() {
+  gallery.innerHTML = '';
   for (let i = 0; i < imageUrls.length; i++) {
     const imageElement = document.createElement('img');
     imageElement.className = 'gallery-image';
@@ -31,4 +35,4 @@ function updateGallery() {
   }
 }
 
-document.addEventListener('DOMContentLoaded', updateGallery);
+updateGallery();
